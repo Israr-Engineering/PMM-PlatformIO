88eaac4116aeeacb630eea5558bfcf7caaa17753
Israr-Engineering <108459666+Israr-Engineering@users.noreply.github.com>
2023-04-25 03:53:21 +0300

    mz12.7

---
+++ Templates/MainG18Av1.6/lib/PmmPWM/PmmPWM.cpp
@@ -0,0 +1,32 @@
+#include <PmmPWM.h>
+
+
+PmmPWMClass::PmmPWMClass() {
+}
+
+PmmPWMClass::~PmmPWMClass() {
+}
+
+void PmmPWMClass::PmmPWMSetup(int PWMOutputArray[], int Resolution)
+{
+    
+    int Length= sizeof(PWMOutputArray) / sizeof(PWMOutputArray[0]);
+    for(int i=0;i<Length;i++)
+    {
+        pinMode(PWMOutputArray[i],OUTPUT);
+    }
+
+    analogWriteResolution(Resolution);
+}
+
+void PmmPWMClass::PmmPWMWrite(uint16_t Value , int pinNumber)
+{
+    analogWrite(pinNumber, Value);
+}
+
+void PmmPWMClass::PmmPWMWritePercentage(int Resolution, int Percentage, int pinNumber)
+{
+    uint16_t FullValue= pow(2,Resolution);
+    uint16_t Value = FullValue * (Percentage/100);
+    analogWrite(pinNumber, Value);
+}
