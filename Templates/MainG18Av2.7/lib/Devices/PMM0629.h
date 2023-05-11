28ec52f6132a28a2ac1191bdf547ef2a453b8cb7
Israr-Engineering <108459666+Israr-Engineering@users.noreply.github.com>
2023-05-11 10:16:30 +0300

    mzz

---
+++ Templates/MainG18Av2.7/lib/Devices/PMM0629.h
@@ -0,0 +1,106 @@
+/* 
+   Board : PMM0620 12x Digital Inputs
+Firmware : 1.1
+    Date : 20230420
+  Auther : MZ
+ */
+#ifndef PMM0628_H
+#define PMM0628_H
+
+#include "Arduino.h"
+#include <PmmTypes.h>
+#include <PmmCommands.h>
+
+extern void ThisProductSetup();
+extern void ThisProductUpdate();
+
+// Start Pin Configration for this board 
+static const uint8_t PMM_DI_Pins[8] = {2ul,3ul,4ul,5ul,6ul,7ul,8ul,9ul};
+static const uint8_t PMM_DO_Pins[4] = {10ul,11ul,12ul,13ul};
+static const uint8_t ETHERRST = 24ul;
+static const uint8_t DIPPROG01 = 46ul ;
+static const uint8_t DIPPROG02 = 47ul  ;
+static const uint8_t DILOSSPOWER = 16ul  ;
+static const uint8_t DIFACTORYRESET = 23ul  ;
+// End Pin Configration for this board 
+
+
+// Call this once at main setup function
+void ThisProductSetup()
+{
+    // configure all pins  as an input and enable the internal pull-up resistor
+    for (uint8_t i = 0; i < 8; i++) pinMode(PMM_DI_Pins[i], INPUT_PULLUP);
+    //STEP03: configure options pin
+    pinMode(DIPPROG01,INPUT);
+    pinMode(DIPPROG02,INPUT);
+    pinMode(DILOSSPOWER,INPUT); 
+    pinMode(DIFACTORYRESET,INPUT);  
+    // Output Pins
+    for (uint8_t i = 0; i < 4; i++) pinMode(PMM_DO_Pins[i], OUTPUT);
+    for (uint8_t i = 0; i < 4; i++) digitalWrite(PMM_DO_Pins[i], LOW);
+  
+
+}
+
+// Manage this in the main loop
+void ThisProductUpdate()
+{
+
+    // save the digital inputs values into a int16 variable 
+    int tmpInputs = 0;
+    String str = "";
+
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        bool sensorVal = digitalRead(PMM_DI_Pins[i]);
+        sensorVal = !sensorVal;
+        if(sensorVal) 
+        {
+            tmpInputs |= (1 << i); //x |= (1 << n); forces nth bit of x to be 1.  all other bits left alone.
+        }
+        else 
+        {
+            tmpInputs &= ~(1 << i); //x &= ~(1 << n);  // forces nth bit of x to be 0.
+        }
+        
+    }
+     
+    // update modbus input register
+    PmmIO.Inputs[0] = tmpInputs; 
+    tmpInputs = 0 ;
+    byte tmpByte = 0;
+
+    bitWrite(tmpByte ,0,digitalRead(DIPPROG01));
+    bitWrite(tmpByte ,1,digitalRead(DIPPROG02));
+    bitWrite(tmpByte ,2,digitalRead(DILOSSPOWER));
+    bitWrite(tmpByte ,3,digitalRead(DIFACTORYRESET));
+
+    tmpInputs = tmpByte;
+    PmmIO.Inputs[1] = tmpInputs; 
+
+    // outputs
+
+    int tmpOutput = PmmIO.Outputs[0] ;
+    SerialUSB.println(tmpOutput);
+
+    for (uint8_t i = 0; i < 4; i++)
+    {
+       
+        if (tmpOutput & 0x0001) 
+        {
+            digitalWrite(PMM_DO_Pins[i] , HIGH);
+        }else{
+            digitalWrite(PMM_DO_Pins[i] , LOW);
+        }
+         tmpOutput =tmpOutput >> 1 ;
+       
+        
+    }
+
+
+
+}
+
+
+
+#endif
